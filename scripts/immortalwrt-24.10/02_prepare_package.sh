4838162bc857528d8d1e04905cf620555ce8886e
marileea <164590361+marileea@users.noreply.github.com>
2025-12-18 23:33:44 +0800

    Update 02_prepare_package.sh

--- scripts/immortalwrt-24.10/02_prepare_package.sh
+++ scripts/immortalwrt-24.10/02_prepare_package.sh
@@ -75,10 +75,35 @@ curl -fsSL https://downloads.immortalwrt.org/releases/${1}/targets/armsr/armv8/p
 cat .vermagic
 sed -i -e 's/^\(.\).*vermagic$/\1cp $(TOPDIR)\/.vermagic $(LINUX_DIR)\/.vermagic/' include/kernel-defaults.mk
 
+# OpenClash
+rm -rf ./feeds/luci/applications/luci-app-openclash
+cp -rf ../openclash/luci-app-openclash ./feeds/luci/applications/luci-app-openclash
+
 # 预配置一些插件
 mkdir -p ./files/etc/uci-defaults
 cp -rf ../files/init/* ./files/
 cp -f ../patch/default-settings/immortalwrt-24.10/zzz-default-settings ./files/etc/uci-defaults/
+
+
+
+mkdir -p files/etc/openclash/core
+
+CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/dev/clash-linux-${1}.tar.gz"
+CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/master/premium\?ref\=core | grep download_url | grep $1 | awk -F '"' '{print $4}')
+CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/core/master/meta/clash-linux-${1}.tar.gz"
+GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
+
+wget -qO- $CLASH_DEV_URL | tar xOvz > files/etc/openclash/core/clash
+wget -qO- $CLASH_TUN_URL | gunzip -c > files/etc/openclash/core/clash_tun
+wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
+wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
+wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
+
+chmod +x files/etc/openclash/core/clash*
+
+
+
 
 # 清理临时文件
 find ./ -name *.orig | xargs rm -f
